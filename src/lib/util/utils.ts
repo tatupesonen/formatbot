b238c8ea2a14077c6f6d8d0a2e6f6b3971265ede
Ben <BenjammingKirby@users.noreply.github.com>
2021-08-16 11:36:37 -0400

    Refactor asyncStringReplacer

--- src/lib/util/utils.ts
+++ src/lib/util/utils.ts
@@ -1,4 +1,19 @@
 /**
+ * @param match The matched substring.
+ * @param offset The offset of the matched substring within the whole string being examined. (For example, if the whole string was 'abcd', and the matched substring was 'bc', then this argument will be 1.)
+ * @param string The whole string being examined.
+ * @param groups In browser versions supporting named capturing groups, will be an object whose keys are the used group names, and whose values are the matched portions (undefined if not matched).
+ * @param capturedGroups An object of named capturing groups whose keys are the names and values are the capturing groups or undefined if no named capturing groups were defined. See [Groups and Ranges](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions/Groups_and_Ranges) for more information.
+ * @returns {string|Promise<string>}
+ */
+ type asyncStringReplacerFn = (
+  match: string,
+  offset: number | undefined,
+  string: string,
+  groups?: { [key: string]: string },
+  ...capturedGroups: Array<string>
+) => string | Promise<string>;
+/**
  * @param stringToReplace The string that'll be replaced
  * @param regex The regex used to match the string, using the global flag "g" is recommended
  * @param callback replacerFunction, this function will be called for every match for the regex and replace it with the result of the callback
@@ -6,18 +21,44 @@
 export async function asyncStringReplacer(
   stringToReplace: string,
   regex: RegExp,
-  callback: (str: string) => string | Promise<string>
+  // TODO add string parameters
+  callback: asyncStringReplacerFn
 ): Promise<string | null> {
   const matched = stringToReplace.match(regex);
-  //If stringToReplace doesn't have a codeblock, return null
+  //If stringToReplace doesn't have a code block, return null
   if (!matched) return null;
-  //Format the matched strings according to the callback
-  const formatted = await Promise.all(matched.map(callback));
-  const result = matched.reduce((prev, curr, ind) => {
-    //The string with the matched values replaced with it's corresponding "formatted string"
-    return prev.replace(curr, formatted[ind]);
-  }, stringToReplace);
-  return result;
+  if (regex.global) {
+    const matches = matched.map((val) => {
+      const singleMatched = val.match(new RegExp(regex.source));
+      // Not possible since it only loops through the matches but for typesript
+      if (!singleMatched) return val;
+      const [match, ...capturedGroup] = singleMatched;
+      return callback(
+        match,
+        singleMatched.index,
+        stringToReplace,
+        singleMatched.groups,
+        ...capturedGroup
+      );
+    });
+    const formatted = await Promise.all(matches);
+
+    const result = matched.reduce((prev, curr, ind) => {
+      //The string with the matched values replaced with it's corresponding "formatted string"
+      return prev.replace(curr, formatted[ind]);
+    }, stringToReplace);
+    return result;
+  } else {
+    const [match, ...capturedGroup] = matched;
+    const replacedValue = await callback(
+      match,
+      matched.index,
+      stringToReplace,
+      matched.groups,
+      ...capturedGroup
+    );
+    return stringToReplace.replace(match, replacedValue);
+  }
 }
 
 /**
