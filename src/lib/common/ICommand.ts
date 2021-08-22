c60af9ed70268ca98ff8e659e5800616133638d9
Tatu Pesonen <pannariiz@gmail.com>
2021-08-22 15:42:48 +0300

    Allow type to receive more arguments

--- src/lib/common/ICommand.ts
+++ src/lib/common/ICommand.ts
@@ -4,7 +4,8 @@ export interface ICommand<T extends keyof typeof COMMAND_TYPE> {
   // Later used for filtering based on the command type
   type: T;
   execute(
-    interaction: T extends COMMAND_TYPE.MESSAGE ? Message : Interaction
+    interaction: T extends COMMAND_TYPE.MESSAGE ? Message : Interaction,
+    args?: string[]
   ): void;
 }
 
