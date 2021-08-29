36bea895ed976ea261aeb13023609a539ce38225
Tatu Pesonen <pannariiz@gmail.com>
2021-08-29 20:36:53 +0300

    Create commit to test workflow

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,7 @@ import { IUploader } from './lib/interfaces/IUploader';
 const bootstrap = async () => {
   const container = new Container();
   // Register dependencies
+  // Test
   container.set<IUploader>(new PastecordImplementation(), DITypes.uploader);
   const { client: bot } = await createBot(container);
   container.set<Client>(bot, DITypes.client);
