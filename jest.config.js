9bdafa0a0fdc87d7fac8c2b56f3f9189990dcc4e
Tatu Pesonen <pannariiz@gmail.com>
2021-09-03 23:54:23 +0300

    ci: add coverage generation

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,10 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['src/**/*.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageReporters: ['html', 'lcov'],
   moduleNameMapper: {
     '@/(.*)': '<rootDir>/src/$1',
     '@tests/(.*)': '<rootDir>/tests/$1',
