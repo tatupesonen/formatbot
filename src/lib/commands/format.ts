0c434c78f17ee2bfb70eea70c0737e83c315d5ff
daniellwdb <44723767+daniellwdb@users.noreply.github.com>
2021-12-01 10:33:24 +0000

    fix: change chat_input to message interaction

--- src/lib/commands/format.ts
+++ src/lib/commands/format.ts
@@ -5,8 +5,7 @@ import { createCommand } from '../util/createCommand';
 
 export default createCommand({
   name: 'Format',
-  description: '',
-  type: 'CHAT_INPUT',
+  type: 'MESSAGE',
   async execute(interaction, container) {
     // Get service
     const service = container.getByKey<FormatService>(DITypes.formatService);
@@ -15,7 +14,7 @@ export default createCommand({
     // Parse package.json first
     try {
       const message = await interaction.channel.messages.fetch(
-        interaction.user.id
+        interaction.targetId
       );
       const formatted = await service.format(message.toString());
       interaction.editReply({ ...baseReply, content: formatted });
