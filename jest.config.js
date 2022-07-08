6ae3fba1846802ad8f269060c06ebb2b725b0c84
Tatu Pesonen <tatu@narigon.dev>
2022-07-09 00:17:42 +0300

    chore(tests): clean up jest config

--- jest.config.js
+++ jest.config.js
@@ -1,16 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  coverageProvider: 'v8',
-  collectCoverageFrom: ['src/**/*.ts'],
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  coverageReporters: ['html', 'lcov'],
-  moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1',
-    '@tests/(.*)': '<rootDir>/tests/$1',
-  },
-  moduleDirectories: ['node_modules', 'src'],
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	modulePathIgnorePatterns: ["<rootDir>/build"]
 };
